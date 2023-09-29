73ce5f60bc30a521ef2ed671be436b56b946033b
Moises Mendez J <moikita_zmen156@hotmail.com>
2023-09-29 12:01:28 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/isc/tesoem/mjm/p2_7s21/PrincipalActivity.java
@@ -0,0 +1,21 @@
+package mx.edu.isc.tesoem.mjm.p2_7s21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+        new Handler().postDelayed(() -> {
+            Intent lanzar = new Intent(PrincipalActivity.this,SegundaActivity.class);
+        startActivity(lanzar);
+        finish();
+        },4000);
+    }
+}
